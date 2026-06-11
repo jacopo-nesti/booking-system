0823c5ee40e7ce80e077bdc7acc09d3d8c7ca4f1
Jacopo Nesti <jacopo.nesti95@gmail.com>
2026-06-11 12:40:57 +0200

    Improve authentication system and fix login/register UX issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from app.database import init_db
 from app.routes import main
+import os
+from dotenv import load_dotenv
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'dev'
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
     app.register_blueprint(main)
     
